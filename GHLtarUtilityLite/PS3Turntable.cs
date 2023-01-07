5b557915dee5443916d2e2141142e0df6644a0bb
shockdude <shockdudex@gmail.com>
2023-01-07 15:24:34 -0800

    turntable LEDs figured out. attempted to hook them to VigEm but it's not working yet

--- GHLtarUtilityLite/PS3Turntable.cs
+++ GHLtarUtilityLite/PS3Turntable.cs
@@ -38,19 +38,45 @@ namespace GHLtarUtilityLite
 
         private void updateRoutine()
         {
+            int bytesRead;
+            var reader = device.OpenEndpointReader(ReadEndpointID.Ep01);
+            byte[] readBuffer = new byte[27];
+
+            UsbSetupPacket setupPacket = new UsbSetupPacket(0x21, 0x09, 0x0201, 0x00, 0x08);
+            int bytesWritten;
+            byte[] writeBuffer = new byte[8];
+            byte buttons = 0x00;
+
+            byte euphoriaState = 0x00;
+            byte playerLEDState = 0x01;
+
+            // FIXME event handler not being called during rumble, probably did something wrong.
+            Xbox360FeedbackReceivedEventHandler onXbox360FeedbackReceived = (sender, eventArgs) =>
+            {
+                if (eventArgs.SmallMotor > 0)
+                {
+                    euphoriaState = 0x01;
+                }
+                else
+                {
+                    euphoriaState = 0x00;
+                }
+                playerLEDState = eventArgs.LedNumber;
+                Console.WriteLine("smallMotor: {0}, playerLED = {1}", eventArgs.SmallMotor, eventArgs.LedNumber);
+            };
+
+            controller.FeedbackReceived += onXbox360FeedbackReceived;
+
             while (!shouldStop)
             {
                 // Read 27 bytes from the turntable
-                int bytesRead;
-                byte[] readBuffer = new byte[27];
-                var reader = device.OpenEndpointReader(ReadEndpointID.Ep01);
                 reader.Read(readBuffer, 100, out bytesRead);
 
                 // Prevent default 0x00 when no bytes are read
                 if (bytesRead > 0)
                 {
                     // Set the table buttons on the virtual 360 controller
-                    byte buttons = readBuffer[0];
+                    buttons = readBuffer[0];
                     controller.SetButtonState(Xbox360Button.A, (buttons & 0x02) != 0x00); // green
                     controller.SetButtonState(Xbox360Button.B, (buttons & 0x04) != 0x00); // red
                     controller.SetButtonState(Xbox360Button.X, (buttons & 0x01) != 0x00); // blue
@@ -79,7 +105,35 @@ namespace GHLtarUtilityLite
                     controller.SetButtonState(Xbox360Button.Down, readBuffer[2] == 3 || readBuffer[2] == 4 || readBuffer[2] == 5);
                     controller.SetButtonState(Xbox360Button.Left, readBuffer[2] == 5 || readBuffer[2] == 6 || readBuffer[2] == 7);
                 }
+
+                // Write 8 bytes to the turntable 2x times
+
+                // Euphoria LED
+                writeBuffer[0] = 0x91;
+                writeBuffer[1] = 0x01;
+                // 1 to enable euphoria LED, 0 to disable euphoria LED
+                writeBuffer[2] = euphoriaState;
+                writeBuffer[3] = 0x00;
+                writeBuffer[4] = 0x00;
+                writeBuffer[5] = 0x00;
+                writeBuffer[6] = 0x00;
+                writeBuffer[7] = 0x00;
+                device.ControlTransfer(ref setupPacket, writeBuffer, 8, out bytesWritten);
+
+                // Player LEDs
+                writeBuffer[0] = 0x01;
+                writeBuffer[1] = 0x08;
+                // 0x1 for P1, 0x2 for P2, 0x4 for P3, 0x8 for P4
+                writeBuffer[2] = playerLEDState;
+                writeBuffer[3] = 0x00;
+                writeBuffer[4] = 0x00;
+                writeBuffer[5] = 0x00;
+                writeBuffer[6] = 0x00;
+                writeBuffer[7] = 0x00;
+                device.ControlTransfer(ref setupPacket, writeBuffer, 8, out bytesWritten);
             }
+
+            controller.FeedbackReceived -= onXbox360FeedbackReceived;
         }
 
         public override void destroy()
